de41d73b9ce1f4f934aa0fbdd02f190c0aff20a5
carlsus <carlsus@gmail.com>
2021-03-03 10:09:18 +0800

    * 20210302090341_FilePath.cs: logo and filepath
    * logo.png:

---
+++ MERMS/Data/Migrations/20210302090341_FilePath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MERMS.Data.Migrations
+{
+    public partial class FilePath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "ApprehensionConfiscations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "ApprehensionConfiscations");
+        }
+    }
+}
